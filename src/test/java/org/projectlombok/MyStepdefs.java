eff5820acc27a7e720976dabb950e0e6bfbce7a8
IgorYarrr <igoryarrroshevich@gmail.com>
2023-01-11 16:14:59 +0300

    homework 2. fixed problem with encoding

---
+++ src/test/java/org/projectlombok/MyStepdefs.java
@@ -0,0 +1,34 @@
+package org.projectlombok;
+
+import io.cucumber.java.ru.Дано;
+import io.cucumber.java.ru.Когда;
+import io.cucumber.java.ru.Тогда;
+import org.junit.Assert;
+
+public class MyStepdefs {
+    String user;
+    String sentMessage;
+    boolean isSent = false;
+    String receivedMessage;
+    @Дано("устанавливаю пользователя {string}")
+    public void устанавливаюПользователя(String arg0) {
+        user = arg0;
+    }
+
+    @Когда("отправляю сообщения в VPS используя {string}")
+    public void отправляюСообщенияВVPSИспользуя(String arg0) {
+        sentMessage = arg0;
+        isSent = true;
+    }
+
+    @Тогда("проверяю приходит ответ сообщение {string}")
+    public void проверяюПриходитОтветСообщение(String arg0) {
+        if (isSent){
+            receivedMessage = "отправлено";
+        }
+        else{
+            receivedMessage = "Error";
+        }
+        Assert.assertEquals(arg0, receivedMessage);
+    }
+}
